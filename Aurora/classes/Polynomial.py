9159dd12f145b0dde4cdb19eaa03e59d704bf005
ahmad-moustapha <ahmad1994@st.ul.edu.lb>
2017-08-15 22:24:09 +0200

    Created by Coding Ground Client

---
+++ Aurora/classes/Polynomial.py
@@ -0,0 +1,16 @@
+class Polynomial:
+  
+  
+  def __init__(self, start, end, coefficients ):
+    self.start = start
+    self.end = end
+    coeffiecents = coeffiecents
+    
+  
+def coxDeBoor(knots, i, p):
+  
+  if p==0:
+    return Polynomial(knots[i], knots[i+1], [1])
+    
+    
+
